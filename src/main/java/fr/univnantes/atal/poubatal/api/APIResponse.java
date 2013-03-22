8439943c56b95f35e1e18216e92f00a42c8bf6ab
Noémi Salaün <noemi.salaun@gmail.com>
2013-03-22 15:55:37 +0100

    Nettoyage des classes

--- src/main/java/fr/univnantes/atal/poubatal/api/APIResponse.java
+++ src/main/java/fr/univnantes/atal/poubatal/api/APIResponse.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package fr.univnantes.atal.poubatal.api;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
